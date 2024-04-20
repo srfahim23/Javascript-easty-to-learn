cc9f72f5378a6f4bd73ac456caaa455877390f94
srfahim <srfahim23@gmail.com>
2024-04-20 19:43:17 +0600

    this is commit

---
+++ index.js
@@ -0,0 +1,100 @@
+// JavaScript (Easy To Learn ) 
+// Learn Beginner to Advance :
+
+// Data Types : 
+// var age = 16; 
+// // var name = "Jane";
+// var name = {first:"jane", last:"Doe"};
+// var truth = false;
+// var sheets = ["HTML", "CSS",  "JS"];
+// var a;typeof a;
+// var a = null 
+// console.log(typeof a);
+
+// Object : 
+// var student ={
+//     firstName : "Jane",
+//     lastName : "Doe",
+//     age :18,
+//     height: 150,
+//     fullName: function(){
+//         return this.firstName  + ""  + this.lastName;
+//     }
+// };
+
+// student.age = 19;
+// student[age]++;
+// name=student.fullName();
+// console.table(studen);
+
+
+// variables: 
+// var a;  // variable 
+// var b   = "init";  // string 
+// var c = "Hi" + "" + "Joe";  // =  "Hi Joe"
+// var d = 1+2+ "3";    // ="33"
+// var e = [2,3,4,7];   // array   
+// var f = false;          // boolean 
+// var g = /()/;        // RegEx 
+// var h = function(){};       // Function object 
+// const PI   = 3.14;      //constant 
+// var a = 1,b =2, c=a+b;   // one line 
+// let z  = "zz";          // block scope local varaible 
+
+// // strickt Mode : 
+// "use strict";           // Use strict mode to write secure code 
+// x = 1;                  // Throws an error becase variable is no 
+
+// console.log(a)
+
+// Variables : 
+
+// Values :    
+false, true                   // boolean 
+18, 3.15, Ob10011, 0xF6, NaN      // number 
+"flower",'john'               // string 
+undefined, null, Infinity   // special 
+
+// Operators : 
+a=b+c-d;       // addition, substraction 
+a=b *(c/d);    // mulitplication, division
+x = 100%48;     // modulo.100/48 remainder = 4
+a++;b--;        // postfix increment and decrement 
+
+// Varialbes :
+
+// Arithmetic : 
+a *(b+c)      // grouping 
+person.age   //member 
+person[age]  // member 
+!(a==b)      // logical not 
+a !=b        // not equal 
+typeof a     // type (number,object, function...)
+x<< 2 x>>3   // minary shifting 
+a = b         // assignment 
+a == b        // equals 
+a != b         //unequal 
+a === b         // strict equal 
+a!==b           // strict unequal 
+a<b a>b         // less and greater than 
+a<=b a>=b       // less or equal  , greater or equal 
+a+=b            // a =a+b (works with   -* %...)
+a && b          // logical and 
+a || b          // logical or 
+
+
+// variables : 
+
+// Bitwise operator : 
+ 
+& AND       // 5 & 1(0101 & 0001) 1(1)
+| OR        // 5 | 1 (0101 | 0001 5(101)
+~ NOT       // ~5(~0101)         10(1010)
+^ XOR       // 5 ^ 1(0101 ^ 0001) 4(100)
+<< left shift // 5<< 1 (0101 <<1) 10(1010)
+>> right shift // 5>> (0101 >> 1) 2(10)
+>>> zero fill right shift // 5>>>1 (0101>>> 1)  2(10)
+
+// Strings : 
+var abc = "abcdefghijklmnopqrstuvwxyz";
+var esce = "I don/'t/n know";
